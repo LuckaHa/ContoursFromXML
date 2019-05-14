5faa4648eaa41ef55017335847f6af2d8f5bd773
xhajduk1 <luciahajdukova@gmail.com>
2019-05-14 08:38:25 +0200

    possibility to run with arguments (specify area, circularity)

--- ContourDrawer/ContourDrawer.py
+++ ContourDrawer/ContourDrawer.py
@@ -1,16 +1,17 @@
 import cv2 as cv
 import numpy as np
+import math
 import os
+import sys
 import shutil
 import xml.etree.ElementTree as ET
 
-idx = 0
-
 '''
 Pre funkcnost programu musia byt subory ulozene podla nasledujucej struktury.
-1. Vsetky mensie obrazky a kontury patriace 1 obrazku su ulozene v osobitnom priecinku.
-2. Tieto priecinky su ocislovane 0,1,2,...
-3. V kazdom z tychto priecinkov su obrazky a .txt subory pomenovane 'a,b'
+1. Vsetko je zabalene do priecinka imgs
+2. Vsetky mensie obrazky a kontury patriace 1 obrazku su ulozene v osobitnom priecinku.
+3. Tieto priecinky su ocislovane 0,1,2,...
+4. V kazdom z tychto priecinkov su obrazky a .txt subory pomenovane 'a,b'
     a - suradnica y v ramci velkeho obrazka
     b - suradnica x v ramci velkeho obrazka
     Napr. 0,1.txt je XML subor s konturami k obrazku 0,1.img
@@ -19,7 +20,23 @@ Obrazky kontur budu ulozene do data/train/label.
 Treningove obrazky budu ulozene do data/test 
 '''
 
-# DRAW CONTOURS
+# PREMENNE
+idx = 0
+from_user = sys.argv
+if (len(from_user) > 1): # od pouzivatela
+    min_area = int(from_user[1])
+    max_area = int(from_user[2])
+    min_circ = float(from_user[3])
+else: # default
+    min_area = 0
+    max_area = 200
+    min_circ = 0.8
+
+print('Minimum cell area: ' + str(min_area))
+print('Maximum cell area: ' + str(max_area))
+print('Minimum cell circularity: ' + str(min_circ))
+
+# METODA NA KRESLENIE KONTUR
 def drawContours(contours, filename):
     cont_img = 255 * np.ones(shape=[512, 512, 3], dtype=np.uint8) # white blank image
     for cnt in contours:
@@ -28,15 +45,18 @@ def drawContours(contours, filename):
 
 # u,v - poloha miniobrazka vramci obrazka
 # od x,y treba odcitat okraje eX,eY a u,v
-def makeContours(folder, filename, v, u, min_area, max_area, min_circ):
+def makeContours(folder, filename, v, u):
     global idx
+    global min_area
+    global max_area
+    global min_circ
     eX = eY = 30 # hranicne body
 
     contours = []
     contours_area = []
     contours_circ = []
 
-    # READ XML & SAVE CONTOURS
+    # PRECITAT XML & ULOZIT KONTURY
     root = ET.parse(folder+'/'+filename).getroot()
     for cont in root.findall('Contours/Contour'):
         contour = []
@@ -44,17 +64,18 @@ def makeContours(folder, filename, v, u, min_area, max_area, min_circ):
             x = int(point.get('x')) - eX - u*512
             y = int(point.get('y')) - eY - v*512
             contour.append([x,y])
-        contour = np.array(contour, dtype=np.int32)
-        contours.append(contour) # add to all contours
+        contour = np.array(contour, dtype=np.int32) # zmenit na array
+        contours.append(contour) # pridat medzi contours
 
-        # filter according to area
+        # filter podla plochy (area)
         area = cv.contourArea(contour)
         if area > min_area and area < max_area:
             contours_area.append(contour)
 
-        # filter according to circularity
-        circ = cv.approxPolyDP(contour,0.01*cv.arcLength(contour,True),True)
-        if len(circ) > min_circ:
+        # filter podla cirkularity
+        perimeter = cv.arcLength(contour,True)
+        circ = 0 if perimeter == 0 else 4*math.pi*(area/(perimeter*perimeter))
+        if min_circ < circ < 1:
             contours_circ.append(contour)
             
     drawContours(contours, 'data/train/label/' + str(idx) + '.png')
@@ -73,7 +94,7 @@ def readXMLfiles(folder):
         else:
             # subor s konturami - vytvoreny obrazok s konturami ulozime do label priecinku pod nazvom idx
             s2 = s1[0].split(',')
-            makeContours(folder, filename, int(s2[0]), int(s2[1]), 100, 300, 10)
+            makeContours(folder, filename, int(s2[0]), int(s2[1]))
             idx += 1
 
 
@@ -86,8 +107,8 @@ def createFolders():
     os.mkdir('data/test')
 
 def moveTestImages():
-    for filename in os.listdir('test'):
-        shutil.copyfile('test/' + filename, 'data/test/' + filename)
+    for filename in os.listdir('imgs/test'):
+        shutil.copyfile('imgs/test/' + filename, 'data/test/' + filename)
 
 def convertTo8b(folder):
     for filename in os.listdir(folder):
@@ -96,9 +117,10 @@ def convertTo8b(folder):
         cv.imwrite(folder+'/'+filename,gray)
 
 # VYTVORIT OBRAZKY BUNIEK A KONTUR
-createFolders
+createFolders()
 for folder in range (6):
-    readXMLfiles(str(folder)) # priecinky su nazvane 0,...5
+    readXMLfiles('imgs/' + str(folder)) # priecinky su nazvane 0,...5
+    print('Image ' + str(folder) + ' of 5 proceeded')
 moveTestImages()
 
 # previest obrazky na ciernobiele 8b
---
+++ ContourDrawer/thresholding.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import os
+import sys
+
+
+def thresholdLabeledImages(folder):
+    idx = 0
+    for filename in os.listdir(folder):
+        if len(filename.split('_')) > 1: # len obrazky _predict chceme previest
+            img = cv.imread(folder + '/' + filename,0)
+
+            # obrazok ma rozmer 256 x 256
+            for i in range(256):
+                for j in range(256):
+                    if img[i,j] > 256*0.9: # prah = 256*0.9
+                        img[i,j] = 255
+                    else:
+                        img[i,j] = 0       
+            cv.imwrite(folder + '/' + str(idx) + 't.png', img)
+            idx += 1
+
+
+from_user = sys.argv
+if (len(from_user) > 1):
+    thresholdLabeledImages(from_user[1]) # bazov priecinka od pouzivatela
+else:
+    thresholdLabeledImages('predicted')
+print('Thresholding finished.')
